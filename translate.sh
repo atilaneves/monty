5f762c710bd666d6012744574ce59123c02cb7c8
Atila Neves <atila.neves@gmail.com>
2022-11-10 18:11:00 +0100

    Remove explicit usage of ldc from translate.sh

--- translate.sh
+++ translate.sh
@@ -8,5 +8,5 @@ o="$PACKAGE_DIR"/source/python/c.d
 
 if [[ ! -f $o || $i -nt $o ]]; then
     PYTHON_INCLUDE_PATH=$(python "$PACKAGE_DIR"/include.py)
-    dub run dpp@0.4.11 --build=release --compiler=ldc2 -- --function-macros --preprocess-only --include-path "$PYTHON_INCLUDE_PATH" "$i"
+    dub run dpp@0.4.11 --build=release -- --function-macros --preprocess-only --include-path "$PYTHON_INCLUDE_PATH" "$i"
 fi
