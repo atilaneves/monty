b21cda7f1b614c7e019e18dedb846a41a4c7202c
Atila Neves <atila.neves@gmail.com>
2022-11-24 14:20:30 -0300

    Make clang mandatory, add echos to translate.sh

--- translate.sh
+++ translate.sh
@@ -10,20 +10,25 @@ if [[ $(which clang) ]]; then
     # Using clang as the linker will cause libclang to be linked even if installed somewhere "weird"
     clinker=clang
 else
-    clinker=cc # just use the default
-    # If clang isn't installed, we still try to find the location of libclang.so
-    libclang=$(find / -name 'libclang.so' 2> /dev/null)
-    if [[ "$?" == 0 ]]; then
-        libclang_dir=$(dirname "$libclang")
-        LIBRARY_PATH="$LIBRARY_PATH:$libclang_dir"
-        LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$libclang_dir"
-    else
-        echo 'Could not find clang or libclang, please install them'
-        exit 1
-    fi
+    # echo "clang not installed, attempting to find libclang.so"
+    # clinker=cc # just use the default
+    # # If clang isn't installed, we still try to find the location of libclang.so
+    # libclang=$(find / -name 'libclang.so' 2> /dev/null)
+    # if [[ "$?" == 0 && $libclang != "" ]]; then
+    #     echo "Found libclang at $libclang"
+    #     libclang_dir=$(dirname "$libclang")
+    #     LIBRARY_PATH="${LIBRARY_PATH:-''}:$libclang_dir"
+    #     LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-''}:$libclang_dir"
+    # else
+    #     echo 'Could not find clang or libclang, please install them'
+    #     exit 1
+    # fi
+    echo "clang must be installed otherwise d++ can't run"
+    exit 1
 fi
 
 if [[ ! -f $o || $i -nt $o ]]; then
     PYTHON_INCLUDE_PATH=$(python3 "$PACKAGE_DIR"/include.py)
+    echo "Translating Python headers in $PYTHON_INCLUDE_PATH"
     CC="$clinker" dub run dpp@0.4.11 --build=release -- --ignore-cursor=stat64 --ignore-cursor=PyType_HasFeature --ignore-cursor=_Py_IS_TYPE  --ignore-cursor=_PyObject_TypeCheck --function-macros --preprocess-only --include-path "$PYTHON_INCLUDE_PATH" "$i"
 fi
