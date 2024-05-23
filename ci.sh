9afacc69488748dc6bdab99603b30e66d05e1e0d
Atila Neves <atila.neves@gmail.com>
2024-05-23 16:53:44 -0500

    Fix Python 3.12

--- ci.sh
+++ ci.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd tests/extensions/raw
+cd "$SCRIPT_DIR"/tests/extensions/raw
+
 if [[ "${OS:-}" == "Windows_NT" ]]; then
     # XXX: hack! on windows one needs to build twice - linker errors occur on the very first monty build
     dub build --compiler=ldc2 || true
@@ -12,17 +13,17 @@ if [[ "${OS:-}" == "Windows_NT" ]]; then
     dub build --compiler=ldc2
     cd -
 
-    cp tests/extensions/raw/raw.dll tests/raw.pyd
+    cp "$SCRIPT_DIR"/tests/extensions/raw/raw.dll "$SCRIPT_DIR"/tests/raw.pyd
     # note: on windows the D shared libraries need to be inside the .pyd directory
     # or python code manually injecting dll paths would need to be used.
-    cp $(where phobos2-ldc-shared.dll) $(where druntime-ldc-shared.dll) tests
+    cp $(where phobos2-ldc-shared.dll) $(where druntime-ldc-shared.dll) "$SCRIPT_DIR"/tests
 
     python -m pytest -s -vv tests
 else
     dub build
     cd -
 
-    cp tests/extensions/raw/libraw.so tests/raw.so
+    cp "$SCRIPT_DIR"/tests/extensions/raw/libraw.so "$SCRIPT_DIR"/tests/raw.so
 
     pytest -s -vv tests
 fi
